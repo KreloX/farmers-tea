2b17f74f10a0e9aeff613a1d04e1d86e92e069a9
KreloX <krelox@seznam.cz>
2025-08-09 17:18:16 +0200

    Initial commit

---
+++ src/main/java/krelox/farmers_tea/FarmersTea.java
@@ -0,0 +1,8 @@
+package krelox.farmers_tea;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FarmersTea.MOD_ID)
+public class FarmersTea {
+    public static final String MOD_ID = "farmers_tea";
+}
